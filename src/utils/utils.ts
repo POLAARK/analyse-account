f9cebef0b595820d2519f493ea6a46537fa88ba0
Bitan <gamespaul@outlook.fr>
2023-05-03 20:59:39 +0200

    Tries with puppeter to avaid rate limit

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,61 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
+import puppeteer from 'puppeteer';
+
+export const fetchhttpJsonPuppeteer = async function(url: string, jsonRequest : string)  {
+
+    // Launch the browser
+    const browser = await puppeteer.launch({headless: 'new'});
+    const page = await browser.newPage();
+
+    // Enable request interception
+    await page.setRequestInterception(true);
+
+    // Listen for 'request' events
+    page.on('request', (request) => {
+        console.log(`Request URL: ${request.url()}`);
+        request.continue();
+        let requestUrl = request.url();
+        if (
+            requestUrl.startsWith('https://api.debank.com/history/list?user_addr=') 
+          //   &&
+          //   contentType &&
+          //   contentType.includes('application/json')
+          ) {
+            console.log(`JSON Response URL: ${requestUrl}`);
+            // console.log(await response.text())
+          //   const content = await response.json();
+          //   console.log(`JSON Response content: ${JSON.stringify(content, null, 2)}`);
+          }
+    });
+
+    // Listen for 'response' events
+    // page.on('response', async (response) => {
+    //     const requestUrl = response.url();
+    //     // const contentType = response.headers()['content-type'];
+    
+    //     // Check if the URL matches the desired pattern and the content type is JSON
+    //     if (
+    //       requestUrl.startsWith('https://api.debank.com/history/list?user_addr=') 
+    //     //   &&
+    //     //   contentType &&
+    //     //   contentType.includes('application/json')
+    //     ) {
+    //       console.log(`JSON Response URL: ${requestUrl}`);
+    //       console.log(await response.text())
+    //     //   const content = await response.json();
+    //     //   console.log(`JSON Response content: ${JSON.stringify(content, null, 2)}`);
+    //     }
+    // });
+
+    // Navigate to the target URL
+    await page.goto(url);
+    new Promise(r => setTimeout(r, 20000));
+    // Close the browser
+    // await browser.close();
+
+}
+
 
 export const fetchJsonFile = function(path: string): any {
     const contentFile = fs.readFileSync(path, 'utf-8');
