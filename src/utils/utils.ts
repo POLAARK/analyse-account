7ba42eaa4094b76f94abfd3ca8d7e2a6a3d42e9c
Bitan <gamespaul@outlook.fr>
2023-05-05 17:07:27 +0200

    Finished scrapping hanfling Rate limite

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,77 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
-import puppeteer from 'puppeteer';
+import puppeteer, { Browser, HTTPResponse, Page } from 'puppeteer';
+import { Http2SecureServer } from 'http2';
 
-export const fetchhttpJsonPuppeteer = async function(url: string, jsonRequest : string)  {
+export const fetchhttpJsonPuppeteer = async function(url: string, jsonRequestUrl : string) : Promise<any> {
+
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    // Enable request interception
+    await page.setRequestInterception(true);
+
+    page.on('request', (request) => {
+        request.continue();
+    });
+    // Listen for 'response' events
+    page.on('response', async (response) => {
+        const requestUrl = response.url();
+        // const contentType = response.headers()['content-type'];
+    
+        // Check if the URL matches the desired pattern and the content type is JSON 'https://api.debank.com/history/list?user_addr='
+        if (
+            requestUrl.startsWith(jsonRequestUrl)
+        ) {
+        try {
+            const content = await response.json();
+            return content;
+        }
+        catch (err) {
+            console.log(' Pref light request Erreur ');
+        }
+        }
+    });
+
+    // Navigate to the target URL
+    await page.goto(url);
+    new Promise(r => setTimeout(r, 10000));
+    // Close the browser
+    // await browser.close();
+
+}
+
+export const fetchhttpJsonPuppeteerTokenAnalysor = async function(url: string, jsonRequestUrl : string,  browser: Browser, page: Page) : Promise<any> {
+    const divSelector = `div[data-id="0xca3f508b8e4dd382ee878a314789373d80a5190a"][data-name="BIFI"][data-token-chain="bsc"]`;
+    await page.setRequestInterception(true);
+
+    // Listen for 'response' events
+    page.on('response', async (response) => {
+        const requestUrl = response.url();
+        // const contentType = response.headers()['content-type'];
+    
+        // Check if the URL matches the desired pattern and the content type is JSON 'https://api.debank.com/history/list?user_addr='
+        if (
+            requestUrl.startsWith(jsonRequestUrl)
+        ) {
+        try {
+            const content = await response.json();
+            return content;
+        }
+        catch (err) {
+            console.log(' Pref light request Erreur ');
+        }
+        }
+    });
+
+    // Navigate to the target URL
+    await page.goto(url);
+    new Promise(r => setTimeout(r, 20000));
+    // Close the browser
+    // await browser.close();
+
+}
+
+export const fetchhttpJsonPuppeteerLoadNewPage = async function(url: string, jsonRequestUrl : string) : Promise<any> {
 
     // Launch the browser
     const browser = await puppeteer.launch({headless: 'new'});
@@ -11,43 +80,22 @@ export const fetchhttpJsonPuppeteer = async function(url: string, jsonRequest : 
     // Enable request interception
     await page.setRequestInterception(true);
 
-    // Listen for 'request' events
-    page.on('request', (request) => {
-        // console.log(`Request URL: ${request.url()}`);
-        request.continue(); 
-        let requestUrl = request.url();
-        if (
-            requestUrl.startsWith('https://api.debank.com/history/list?user_addr=') 
-          //   &&
-          //   contentType &&
-          //   contentType.includes('application/json')
-          ) {
-            console.log(`JSON Response URL: ${request.response()}`);
-          }
-    });
-
     // Listen for 'response' events
     page.on('response', async (response) => {
         const requestUrl = response.url();
         // const contentType = response.headers()['content-type'];
     
-        // Check if the URL matches the desired pattern and the content type is JSON
+        // Check if the URL matches the desired pattern and the content type is JSON 'https://api.debank.com/history/list?user_addr='
         if (
-          requestUrl.startsWith('https://api.debank.com/history/list?user_addr=') 
-        //   &&
-        //   contentType &&
-        //   contentType.includes('application/json')
+            requestUrl.startsWith(jsonRequestUrl)
         ) {
-          console.log(`JSON Response URL: ${requestUrl}`);
-        //   console.log(response);
         try {
             const content = await response.json();
-          console.log(content);
+            return content;
         }
         catch (err) {
             console.log(' Pref light request Erreur ');
         }
-        //   console.log(`JSON Response content: ${JSON.stringify(content, null, 2)}`);
         }
     });
 
