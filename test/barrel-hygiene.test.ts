b2d0faf21e5026b57a52a42a6d7038598ba47808
Paul <gamespaul@outlook.fr>
2026-08-27 01:49:24 +0200

    hygiene: remove dead fetchUtils exports and stray console.log

---
+++ test/barrel-hygiene.test.ts
@@ -0,0 +1,30 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+import * as utilsBarrel from "../src/utils";
+
+const deletedExports = [
+  "fetchhttpJsonPuppeteer",
+  "fetchhttpJsonPuppeteerTokenAnalysor",
+  "fetchhttpJsonPuppeteerLoadNewPage",
+  "fetchJsonFile",
+  "fetchHttpJsonHandlingTooManyRequest",
+  "makeDirectory",
+  "writeFile",
+  "isOccurenceInString",
+] as const;
+
+test("utils module no longer exports removed dead helpers", () => {
+  for (const name of deletedExports) {
+    assert.equal(
+      (utilsBarrel as Record<string, unknown>)[name],
+      undefined,
+      `expected ${name} to be absent from src/utils barrel exports`,
+    );
+  }
+});
+
+test("surviving fetch utilities remain exported", () => {
+  assert.equal(typeof utilsBarrel.fetchHttp, "function");
+  assert.equal(typeof utilsBarrel.fetchHttpJson, "function");
+  assert.equal(typeof utilsBarrel.HttpResponseError, "function");
+});
